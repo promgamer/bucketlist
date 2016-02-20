baeedd70e19a12ad2c3e3421920dc4f271b21808
mspkvp <msp4ever@gmail.com>
2016-02-20 17:17:49 +0000

    new frontend seed

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bucketlistApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bucketlistApp
+ */
+angular.module('bucketlistApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bucketlistApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bucketlistApp
+ */
+angular.module('bucketlistApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
